7985cae6402bd86396cdd30664842f75a0768b19
ridho muhammad <ulunmuhammadridho@gmail.com>
2020-03-24 22:17:14 +0700

    initial commit

---
+++ scrapper-covid-banua-news/index.js
@@ -0,0 +1,52 @@
+// Package Dependencies
+const puppeteer = require('puppeteer');
+
+// Primitive Constants
+const WEBSITE_TARGET = 'http://covid19.kopibanjarmasin.com/covidkalsel.php'
+
+// Selector Constants
+const $containerSelector = ".mengantisipasi > .anticipate > .container > .content-area >"
+const $casesSelector = ".content-list > .list-flat .col-6 .desc"
+const $citySelector = ".list-flat .col-6 .desc"
+
+/**
+ * Create city object based on name and total
+ * @param {string} cityName 
+ * @param {total} total 
+ */
+const createCityData = (name, total) => ({
+    name,
+    total
+});
+
+const setLabelAndValueFromProperties = async ($properties, cb) => {
+    for (const $prop of $properties) {
+        // Get label and value
+        const label = await $prop.$eval('h5:nth-child(1)', i => i.innerHTML);
+        const value = await $prop.$eval('h5:nth-child(2)', i => i.innerHTML);
+    
+        cb(label, value)
+      }
+}
+
+
+(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  let results = {totals:{},cities:[]};
+
+  await page.goto(WEBSITE_TARGET);
+
+   let $cases = await page.$$(`${$containerSelector} ${$casesSelector}`)
+   let $cities = await page.$$(`${$containerSelector} ${$citySelector}`);
+
+   await setLabelAndValueFromProperties($cases, (label, value) => results.totals[label] = value)
+   await setLabelAndValueFromProperties($cities, (label, value) => results.cities.push(createCityData(label, value)))
+
+   console.log("resultnya: ", results)
+
+
+
+
+  await browser.close();
+})();
